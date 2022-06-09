43dbca1bdb0e67c8b645c09c83e52ba794b9274e
Gugustinette <mercier.augustin@outlook.fr>
2022-06-09 21:09:48 +0200

    UnInstall + webextension-polyfill

---
+++ scripts/uninstall.sh
@@ -0,0 +1,14 @@
+# uninstall script
+
+# webpack-extension-reloader
+rm -rf ./lib/vue-cli-plugin-browser-extension/lib/webpack-extension-reloader-fix_issue_125/node_modules
+rm -rf ./lib/vue-cli-plugin-browser-extension/lib/webpack-extension-reloader-fix_issue_125/dist
+rm ./lib/vue-cli-plugin-browser-extension/lib/webpack-extension-reloader-fix_issue_125/package-lock.json
+
+# vue-cli-plugin-browser-extension
+rm -rf ./lib/vue-cli-plugin-browser-extension/node_modules
+rm ./lib/vue-cli-plugin-browser-extension/package-lock.json
+
+# eris
+rm -rf ./node_modules
+rm ./package-lock.json
