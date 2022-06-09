2782f0f4f5c94eac85efd557e90503b019305f0a
Gugustinette <mercier.augustin@outlook.fr>
2022-06-09 20:36:47 +0200

    Untracked files issue resolved to fix .gitignore

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,14 @@
 # Install script
-cd ./lib/vue-cli-plugin-browser-extension
+
+# webpack-extension-reloader
+cd ./lib/vue-cli-plugin-browser-extension/lib/webpack-extension-reloader-fix_issue_125
 npm install
+npm run build
+
+# vue-cli-plugin-browser-extension
+cd ../../
+npm install
+
+# eris
 cd ../../
 npm install
